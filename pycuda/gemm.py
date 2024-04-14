355c548c19b7353e24ae92ac985f17c1a1378db0
Ziliang Peng <ziliangdotme@gmail.com>
2024-04-14 07:18:34 +0000

    time it

--- pycuda/gemm.py
+++ pycuda/gemm.py
@@ -1,7 +1,22 @@
+import time
 import pycuda.autoinit
 import pycuda.driver as drv
 import numpy as np
 from pycuda.compiler import SourceModule
+
+
+class Timed:
+    def __init__(self, name=""):
+        self.name = name
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end = time.time()
+        self.elapsed = self.end - self.start
+        print(f"{self.name} elapsed time: {self.elapsed} seconds")
 
 
 # Define the CUDA kernel as a string.
@@ -25,7 +40,7 @@ mod = SourceModule(kernel_code)
 gemm = mod.get_function("gemm")
 
 # Define the size of the matrices.
-N = 32  # Size of the matrix (N x N).
+N = 1024 * 2  # Size of the matrix (N x N).
 
 # Create random matrices A and B.
 A = np.random.randn(N, N).astype(np.float32)
@@ -39,10 +54,17 @@ block_size = (8, 8, 1)  # Threads per block (8x8).
 grid_size = (int(np.ceil(N / block_size[0])), int(np.ceil(N / block_size[1])))
 
 # Launch the kernel.
-gemm(
-    drv.In(A), drv.In(B), drv.Out(C_cuda), np.int32(N), block=block_size, grid=grid_size
-)
+with Timed("GPU"):
+    gemm(
+        drv.In(A),
+        drv.In(B),
+        drv.Out(C_cuda),
+        np.int32(N),
+        block=block_size,
+        grid=grid_size,
+    )
 
-C_cpu = np.matmul(A, B)
+with Timed("CPU"):
+    C_cpu = np.matmul(A, B)
 
 print("All close:", np.allclose(C_cuda, C_cpu, rtol=1e-3, atol=1e-5))
