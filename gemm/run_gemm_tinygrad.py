265a45d49ab71b7b30c3916f1fb3d76034087048
Ziliang Peng <ziliangdotme@gmail.com>
2024-04-21 18:22:13 +0000

    tinygrad gemm +

--- gemm/run_gemm_tinygrad.py
+++ gemm/run_gemm_tinygrad.py
@@ -4,6 +4,6 @@ N = 1024
 
 a, b = Tensor.rand(N, N), Tensor.rand(N, N)
 
-c = a @ b
-
+# c = a @ b # typical matmul.
+c = (a.reshape(N, 1, N) * b.T.reshape(1, N, N)).sum(axis=2); # interesting matmul.
 print(c.numpy().mean())
