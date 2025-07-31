c5ad3b67773c82b229713fc6c854539805c1341a
Ziliang Peng <ziliangdotme@gmail.com>
2025-07-30 23:43:36 -0700

    -

--- torch/grad.py
+++ torch/grad.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 import time
 from loguru import logger
 
+
 def cross_entropy(logits, label):
     target = F.one_hot(torch.tensor([label]), num_classes=10)
     logits_softmax = F.softmax(logits, dim=-1)
@@ -10,10 +11,8 @@ def cross_entropy(logits, label):
     return loss
 
 
-
-
 def main():
-    a = torch.randn(1, 10, requires_grad=True)#.to('mps:0')
+    a = torch.randn(1, 10, requires_grad=True)  # .to('mps:0')
     # optimizer = torch.optim.SGD([a], lr=0.001)
     optimizer = torch.optim.Adam([a], lr=0.001)
 
@@ -25,7 +24,8 @@ def main():
         loss.backward()
         optimizer.step()
         if i % 1000000 == 0:
-            logger.info(f'{a[0][label].item()}')
+            logger.info(f"{a[0][label].item()}")
             # print(loss.item())
+
 
 main()
