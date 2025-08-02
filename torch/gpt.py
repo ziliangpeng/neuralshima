b5bba11918989241084dde1eb87aeed86d8b65fc
Ziliang Peng <ziliangdotme@gmail.com>
2025-08-02 11:15:42 -0700

    optimize mask; diverse training data; multi type attention

--- torch/gpt.py
+++ torch/gpt.py
@@ -19,13 +19,14 @@ class Block(nn.Module):
     def __call__(self, x):
         return self.forward(x)
 
-class Attention(nn.Module):
+class MHAttention(nn.Module):
     def __init__(self, dim, n_heads):
         super().__init__()
         self.n_heads = n_heads
         self.dim = dim
         self.proj = nn.Linear(dim, 3 * dim, device=dv)
         self.proj2 = nn.Linear(dim, dim, device=dv)
+        self.register_buffer('mask', torch.tril(torch.ones(1024, 1024, device=dv)).unsqueeze(0).unsqueeze(0))
         # self.ln = nn.LayerNorm(dim)
 
     def forward(self, x):
@@ -36,17 +37,32 @@ class Attention(nn.Module):
         v = v.view(B, T, self.n_heads, self.dim // self.n_heads).transpose(1, 2) # B, n_heads, T, head_dim
 
         qk = q @ k.transpose(-2, -1) / math.sqrt(k.size(-1))
-        qk.masked_fill(torch.tril(torch.ones(T, T, device=dv)) == 0, float('-inf'))
+        qk.masked_fill(self.mask[:, :, :T, :T] == 0, float('-inf'))
         att = F.softmax(qk, dim=-1) @ v # (B, n_heads, T, head_dim)
         att = att.transpose(1, 2).reshape(B, T, D)
         return self.proj2(att)
 
+class MQAttention(nn.Module):
+    pass
+
+class GQAttention(nn.Module):
+    pass
+
 class TransformerBlock(nn.Module):
-    def __init__(self, dim, n_heads):
+    def __init__(self, dim, n_heads, att_type):
         super().__init__()
         assert dim % n_heads == 0
+        if att_type == 'mha':
+            self.attn_type = MHAttention
+        elif att_type == 'mqa':
+            self.attn_type = MQAttention
+        elif att_type == 'gqa':
+            self.attn_type = GQAttention
+        else:
+            raise Exception(f'Invalid attention type: {att_type}')
+
         self.ln1 = nn.LayerNorm(dim, device=dv)
-        self.attn = Attention(dim, n_heads)
+        self.attn = self.attn_type(dim, n_heads)
         self.ln2 = nn.LayerNorm(dim, device=dv)
         # self.mlp = nn.FeedForward(dim, 4 * dim)
         self.proj1 = nn.Linear(dim, 4 * dim, device=dv)
@@ -63,18 +79,19 @@ class TransformerBlock(nn.Module):
 class GPT(nn.Module):
     def __init__(self, vocab_size, dim, n_heads, n_layers, seq_len):
         super().__init__()
+        attn_type = 'mha'
         self.vocab_size = vocab_size
         # Convert tensors to nn.Parameter so they get tracked by the optimizer
         self.pos_emb = nn.Parameter(torch.randn(seq_len, dim, device=dv))
         self.tok_emb = nn.Parameter(torch.randn(vocab_size, dim, device=dv))
         # Use ModuleList instead of regular list so parameters are registered
         # self.blocks = nn.ModuleList([Block(dim) for i in range(n_layers)])
-        self.blocks = nn.ModuleList([TransformerBlock(dim, n_heads) for i in range(n_layers)])
+        self.blocks = nn.ModuleList([TransformerBlock(dim, n_heads, attn_type) for i in range(n_layers)])
         self.ln = nn.LayerNorm(dim, device=dv)
         self.proj = nn.Linear(dim, vocab_size, device=dv)
 
     def cross_entropy(self, logits, label, num_classes):
-        target = F.one_hot(torch.tensor(label), num_classes).to(dv)
+        target = F.one_hot(label, num_classes).to(dv)
 
         # logits_softmax = F.softmax(logits, dim=-1)
         # logits_log_softmax = torch.log(logits_softmax)
@@ -158,9 +175,9 @@ def main():
     # for p in g.parameters():
     #     print('--')
     #     print(p.shape)
-    input, target = dataloader.next_batch(batch_size)
     sgd = torch.optim.Adam(params=list(g.parameters()), lr=3e-4)
     for it in range(1000000000):
+        input, target = dataloader.next_batch(batch_size)
         sgd.zero_grad()
         logits, loss = g(input, target)
         if it % 1000 == 0:
