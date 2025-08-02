d37404a93262ea33949e419d7ee3d3e853c05c45
Ziliang Peng <ziliangdotme@gmail.com>
2025-08-02 10:16:10 -0700

    use log_softmax

--- torch/grad.py
+++ torch/grad.py
@@ -6,25 +6,38 @@ from loguru import logger
 
 def cross_entropy(logits, label):
     target = F.one_hot(torch.tensor([label]), num_classes=10)
-    logits_softmax = F.softmax(logits, dim=-1)
-    loss = -torch.sum(torch.log(logits_softmax) * target)
+
+    # logits_softmax = F.softmax(logits, dim=-1)
+    # logits_log_softmax = torch.log(logits_softmax)
+
+    logits_log_softmax = torch.log_softmax(logits, dim=-1)
+    loss = -torch.sum(logits_log_softmax * target)
     return loss
 
 
 def main():
     a = torch.randn(1, 10, requires_grad=True)  # .to('mps:0')
-    # optimizer = torch.optim.SGD([a], lr=0.001)
-    optimizer = torch.optim.Adam([a], lr=0.001)
+    optimizer = torch.optim.SGD([a], lr=0.001)
+    # optimizer = torch.optim.Adam([a], lr=0.001)
 
     label = 0
     for i in range(1000000000):
-        optimizer.zero_grad()
-        b = a / 10.0
+        if i % 10000 == 0:
+            logger.info(f"{a[0][label].item()}")
+        # optimizer.zero_grad()
+        if a.grad is not None:
+            a.grad.zero_()
+
+        # b = torch.exp(a)
+        b = (1 - a) * 100
+        # b = a
         loss = cross_entropy(b, label)
         loss.backward()
-        optimizer.step()
-        if i % 1000000 == 0:
-            logger.info(f"{a[0][label].item()}")
+        with torch.no_grad():
+            a.sub_(a.grad, alpha=0.001)
+            # a -= a.grad * 0.001
+        # a = a + a.grad.detach() * 0.001
+        # optimizer.step()
             # print(loss.item())
 
 
